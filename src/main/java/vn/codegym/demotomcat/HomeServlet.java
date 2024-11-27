715404f43a62c401a892971cc34c286b62e52698
riskgame15 <tttungcr@gmail.com>
2024-11-27 10:42:23 +0700

    demoTomcat

---
+++ src/main/java/vn/codegym/demotomcat/HomeServlet.java
@@ -0,0 +1,35 @@
+package vn.codegym.demotomcat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", urlPatterns = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = req.getPathInfo();
+        System.out.println(url);
+
+        resp.setContentType("text/html; charset=utf-8" );
+        resp.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
